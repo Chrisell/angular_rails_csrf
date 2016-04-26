f7e1a2639c62171cf6013f3cd141cf91eccd8077
Chris Ell <Christopher_Ell@cable.comcast.com>
2016-04-26 10:26:53 -0400

    Makes cookie secure in all environments except local development

--- lib/angular_rails_csrf/concern.rb
+++ lib/angular_rails_csrf/concern.rb
@@ -7,7 +7,12 @@ module AngularRailsCsrf
     end
 
     def set_xsrf_token_cookie
-      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+      if protect_against_forgery?
+        cookies['XSRF-TOKEN'] = {
+          value: form_authenticity_token,
+          secure: !Rails.env.dev?
+        }
+      end
     end
 
     def verified_request?
